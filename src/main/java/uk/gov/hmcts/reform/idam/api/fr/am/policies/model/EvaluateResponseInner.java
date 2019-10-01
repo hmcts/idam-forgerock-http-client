0cf13a75488390082354346e41931dd9f78fb4ba
Tiago Braun <tiago.braun@amido.com>
2019-10-01 15:18:29 +0100

    feat(SIDM-3147-pol-post): v1.7.2 with policy/post

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/policies/model/EvaluateResponseInner.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/policies/model/EvaluateResponseInner.java
@@ -21,7 +21,7 @@ import java.util.Objects;
 /**
  * EvaluateResponseInner
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-09-29T17:27:31.901+01:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-10-01T14:14:33.256+01:00")
 public class EvaluateResponseInner {
   @JsonProperty("resource")
   private String resource = null;
