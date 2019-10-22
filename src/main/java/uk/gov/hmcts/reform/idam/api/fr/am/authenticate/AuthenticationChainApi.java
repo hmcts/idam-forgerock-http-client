065e3206d6f42c6c853c3168b6e1a8c43c5124fb
Tiago Braun <tiago.braun@amido.com>
2019-10-22 16:33:34 +0100

    feat(SIDM-3293-auth-api): v1.8.0 with auth chain api

---
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.reform.idam.api.fr.am.authenticate;
+
+import feign.Headers;
+import feign.Param;
+import feign.QueryMap;
+import feign.RequestLine;
+import uk.gov.hmcts.reform.idam.api.fr.am.authenticate.model.AuthenticationChain;
+import uk.gov.hmcts.reform.idam.api.fr.client.invoker.ApiClient;
+import uk.gov.hmcts.reform.idam.api.fr.client.invoker.EncodingUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-10-22T16:27:36.995+01:00")
+public interface AuthenticationChainApi extends ApiClient.Api {
+
+
+  /**
+   * Submits an authentication chain or blank if initiating the chain
+   * POST authentication chain
+    * @param realm Parameter specifying the realm. (required)
+    * @param authIndexType Specifies the indexType, such as service (required)
+    * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
+    * @param requestPayload  (optional)
+   * @return AuthenticationChain
+   */
+  @RequestLine("POST /json/realms/{realm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+  @Headers({
+    "Content-Type: application/json",
+    "Accept: application/json",
+  })
+  AuthenticationChain authenticate(@Param("realm") String realm,
+                                   @Param("authIndexType") String authIndexType,
+                                   @Param("authIndexValue") String authIndexValue,
+                                   AuthenticationChain requestPayload);
+
+  /**
+   * Submits an authentication chain or blank if initiating the chain
+   * POST authentication chain
+   * Note, this is equivalent to the other <code>authenticate</code> method,
+   * but with the query parameters collected into a single Map parameter. This
+   * is convenient for services with optional query parameters, especially when
+   * used with the {@link AuthenticateQueryParams} class that allows for
+   * building up this map in a fluent style.
+   * @param realm Parameter specifying the realm. (required)
+   * @param requestPayload  (optional)
+   * @param queryParams Map of query parameters as name-value pairs
+   *   <p>The following elements may be specified in the query map:</p>
+   *   <ul>
+   *   <li>authIndexType - Specifies the indexType, such as service (required)</li>
+   *   <li>authIndexValue - Specifies the identity of the module we are calling, such as hotpChain (required)</li>
+   *   </ul>
+   * @return AuthenticationChain
+   */
+  @RequestLine("POST /json/realms/{realm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+  @Headers({
+  "Content-Type: application/json",
+  "Accept: application/json",
+  })
+  AuthenticationChain authenticate(@Param("realm") String realm,
+                                   AuthenticationChain requestPayload,
+                                   @QueryMap(encoded = true) Map<String, Object> queryParams);
+
+  /**
+   * A convenience class for generating query parameters for the
+   * <code>authenticate</code> method in a fluent style.
+   */
+  public static class AuthenticateQueryParams extends HashMap<String, Object> {
+    public AuthenticateQueryParams authIndexType(final String value) {
+      put("authIndexType", EncodingUtils.encode(value));
+      return this;
+    }
+    public AuthenticateQueryParams authIndexValue(final String value) {
+      put("authIndexValue", EncodingUtils.encode(value));
+      return this;
+    }
+  }
+}
