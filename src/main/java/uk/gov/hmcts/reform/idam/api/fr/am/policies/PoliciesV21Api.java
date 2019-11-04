38f3e0e5a5ec58db62cbace78e4b2207c1f8c674
Tiago Braun <tiago.braun@amido.com>
2019-11-04 14:25:49 +0000

    feat(SIDM-3324-getall-pols): PoliciesAPI: add get all policies

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/policies/PoliciesV21Api.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/policies/PoliciesV21Api.java
@@ -12,13 +12,14 @@ import uk.gov.hmcts.reform.idam.api.fr.am.policies.model.PostRequest;
 import uk.gov.hmcts.reform.idam.api.fr.am.policies.model.PostResponse;
 import uk.gov.hmcts.reform.idam.api.fr.am.policies.model.PutRequest;
 import uk.gov.hmcts.reform.idam.api.fr.am.policies.model.PutResponse;
+import uk.gov.hmcts.reform.idam.api.fr.am.policies.model.QueryResponse;
 import uk.gov.hmcts.reform.idam.api.fr.client.invoker.ApiClient;
 import uk.gov.hmcts.reform.idam.api.fr.client.invoker.EncodingUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-10-04T13:59:55.834+01:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-11-04T13:43:51.856Z")
 public interface PoliciesV21Api extends ApiClient.Api {
 
 
@@ -40,7 +41,12 @@ public interface PoliciesV21Api extends ApiClient.Api {
     
     "Cookie: {cookie}"
   })
-  DeleteResponse delete(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("fields") String fields, @Param("prettyPrint") Boolean prettyPrint, @Param("cookie") String cookie);
+  DeleteResponse delete(@Param("realm") String realm,
+                        @Param("resourceId") String resourceId,
+                        @Param("acceptAPIVersion") String acceptAPIVersion,
+                        @Param("fields") String fields,
+                        @Param("prettyPrint") Boolean prettyPrint,
+                        @Param("cookie") String cookie);
 
   /**
    * Delete
@@ -66,10 +72,14 @@ public interface PoliciesV21Api extends ApiClient.Api {
   @Headers({
   "Accept: application/json",
       "Accept-API-Version: {acceptAPIVersion}",
-      
+
       "Cookie: {cookie}"
   })
-  DeleteResponse delete(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("cookie") String cookie, @QueryMap(encoded = true) Map<String, Object> queryParams);
+  DeleteResponse delete(@Param("realm") String realm,
+                        @Param("resourceId") String resourceId,
+                        @Param("acceptAPIVersion") String acceptAPIVersion,
+                        @Param("cookie") String cookie,
+                        @QueryMap(encoded = true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -106,7 +116,13 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
     "Accept-API-Version: {acceptAPIVersion}"
   })
-  EvaluateResponse evaluate(EvaluateRequest requestPayload, @Param("action") String action, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("fields") String fields, @Param("prettyPrint") Boolean prettyPrint, @Param("realm") String realm, @Param("cookie") String cookie);
+  EvaluateResponse evaluate(EvaluateRequest requestPayload,
+                            @Param("action") String action,
+                            @Param("acceptAPIVersion") String acceptAPIVersion,
+                            @Param("fields") String fields,
+                            @Param("prettyPrint") Boolean prettyPrint,
+                            @Param("realm") String realm,
+                            @Param("cookie") String cookie);
 
   /**
    * Policies: Action: evaluate
@@ -137,7 +153,10 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
       "Accept-API-Version: {acceptAPIVersion}"
   })
-  EvaluateResponse evaluate(EvaluateRequest requestPayload, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("cookie") String cookie, @QueryMap(encoded = true) Map<String, Object> queryParams);
+  EvaluateResponse evaluate(EvaluateRequest requestPayload,
+                            @Param("acceptAPIVersion") String acceptAPIVersion,
+                            @Param("cookie") String cookie,
+                            @QueryMap(encoded = true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -181,7 +200,13 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
     "Cookie: {cookie}"
   })
-  GetResponse get(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("fields") String fields, @Param("prettyPrint") Boolean prettyPrint, @Param("mimeType") String mimeType, @Param("cookie") String cookie);
+  GetResponse get(@Param("realm") String realm,
+                  @Param("resourceId") String resourceId,
+                  @Param("acceptAPIVersion") String acceptAPIVersion,
+                  @Param("fields") String fields,
+                  @Param("prettyPrint") Boolean prettyPrint,
+                  @Param("mimeType") String mimeType,
+                  @Param("cookie") String cookie);
 
   /**
    * Read
@@ -211,7 +236,11 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
       "Cookie: {cookie}"
   })
-  GetResponse get(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, @Param("cookie") String cookie, @QueryMap(encoded = true) Map<String, Object> queryParams);
+  GetResponse get(@Param("realm") String realm,
+                  @Param("resourceId") String resourceId,
+                  @Param("acceptAPIVersion") String acceptAPIVersion,
+                  @Param("cookie") String cookie,
+                  @QueryMap(encoded = true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -251,7 +280,12 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
     "Cookie: {cookie}"
   })
-  PostResponse post(@Param("realm") String realm, @Param("acceptAPIVersion") String acceptAPIVersion, PostRequest requestPayload, @Param("fields") String fields, @Param("prettyPrint") Boolean prettyPrint, @Param("cookie") String cookie);
+  PostResponse post(@Param("realm") String realm,
+                    @Param("acceptAPIVersion") String acceptAPIVersion,
+                    PostRequest requestPayload,
+                    @Param("fields") String fields,
+                    @Param("prettyPrint") Boolean prettyPrint,
+                    @Param("cookie") String cookie);
 
   /**
    * Create with Server-Assigned ID
@@ -281,7 +315,11 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
       "Cookie: {cookie}"
   })
-  PostResponse post(@Param("realm") String realm, @Param("acceptAPIVersion") String acceptAPIVersion, PostRequest requestPayload, @Param("cookie") String cookie, @QueryMap(encoded = true) Map<String, Object> queryParams);
+  PostResponse post(@Param("realm") String realm,
+                    @Param("acceptAPIVersion") String acceptAPIVersion,
+                    PostRequest requestPayload,
+                    @Param("cookie") String cookie,
+                    @QueryMap(encoded = true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -318,7 +356,13 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
     "Cookie: {cookie}"
   })
-  PutResponse put(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, PutRequest requestPayload, @Param("fields") String fields, @Param("prettyPrint") Boolean prettyPrint, @Param("cookie") String cookie);
+  PutResponse put(@Param("realm") String realm,
+                  @Param("resourceId") String resourceId,
+                  @Param("acceptAPIVersion") String acceptAPIVersion,
+                  PutRequest requestPayload,
+                  @Param("fields") String fields,
+                  @Param("prettyPrint") Boolean prettyPrint,
+                  @Param("cookie") String cookie);
 
   /**
    * Create Policy with Client-Assigned ID
@@ -349,7 +393,12 @@ public interface PoliciesV21Api extends ApiClient.Api {
 
       "Cookie: {cookie}"
   })
-  PutResponse put(@Param("realm") String realm, @Param("resourceId") String resourceId, @Param("acceptAPIVersion") String acceptAPIVersion, PutRequest requestPayload, @Param("cookie") String cookie, @QueryMap(encoded = true) Map<String, Object> queryParams);
+  PutResponse put(@Param("realm") String realm,
+                  @Param("resourceId") String resourceId,
+                  @Param("acceptAPIVersion") String acceptAPIVersion,
+                  PutRequest requestPayload,
+                  @Param("cookie") String cookie,
+                  @QueryMap(encoded = true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -362,6 +411,103 @@ public interface PoliciesV21Api extends ApiClient.Api {
     }
     public PutQueryParams prettyPrint(final Boolean value) {
       put("_prettyPrint", EncodingUtils.encode(value));
+      return this;
+    }
+  }
+
+  /**
+   * Query by Filter
+   * Query the stored policies
+    * @param realm Parameter specifying the realm. (required)
+    * @param queryFilter  (required)
+    * @param acceptAPIVersion  (required)
+    * @param fields Optional parameter containing a comma separated list of field references specifying which fields of the targeted JSON resource should be returned. (optional)
+    * @param prettyPrint Optional parameter requesting that the returned JSON resource content should be formatted to be more human readable. (optional)
+    * @param pageSize  (optional)
+    * @param totalPagedResultsPolicy  (optional)
+    * @param sortKeys  (optional)
+    * @param cookie  (optional)
+   * @return QueryResponse
+   */
+  @RequestLine("GET /json/realms/{realm}/policies?_fields={fields}&_prettyPrint={prettyPrint}&_queryFilter={queryFilter}&_pageSize={pageSize}&_totalPagedResultsPolicy={totalPagedResultsPolicy}&_sortKeys={sortKeys}")
+  @Headers({
+    "Accept: application/json",
+    "Accept-API-Version: {acceptAPIVersion}",
+
+    "Cookie: {cookie}"
+  })
+  QueryResponse query(@Param("realm") String realm,
+                      @Param("queryFilter") String queryFilter,
+                      @Param("acceptAPIVersion") String acceptAPIVersion,
+                      @Param("fields") String fields,
+                      @Param("prettyPrint") Boolean prettyPrint,
+                      @Param("pageSize") Integer pageSize,
+                      @Param("totalPagedResultsPolicy") String totalPagedResultsPolicy,
+                      @Param("sortKeys") String sortKeys,
+                      @Param("cookie") String cookie);
+
+  /**
+   * Query by Filter
+   * Query the stored policies
+   * Note, this is equivalent to the other <code>query</code> method,
+   * but with the query parameters collected into a single Map parameter. This
+   * is convenient for services with optional query parameters, especially when
+   * used with the {@link QueryQueryParams} class that allows for
+   * building up this map in a fluent style.
+   * @param realm Parameter specifying the realm. (required)
+   * @param acceptAPIVersion  (required)
+   * @param cookie  (optional)
+   * @param queryParams Map of query parameters as name-value pairs
+   *   <p>The following elements may be specified in the query map:</p>
+   *   <ul>
+   *   <li>fields - Optional parameter containing a comma separated list of field references specifying which fields of the targeted JSON resource should be returned. (optional)</li>
+   *   <li>prettyPrint - Optional parameter requesting that the returned JSON resource content should be formatted to be more human readable. (optional)</li>
+   *   <li>queryFilter -  (required)</li>
+   *   <li>pageSize -  (optional)</li>
+   *   <li>totalPagedResultsPolicy -  (optional)</li>
+   *   <li>sortKeys -  (optional)</li>
+   *   </ul>
+   * @return QueryResponse
+   */
+  @RequestLine("GET /json/realms/{realm}/policies?_fields={fields}&_prettyPrint={prettyPrint}&_queryFilter={queryFilter}&_pageSize={pageSize}&_totalPagedResultsPolicy={totalPagedResultsPolicy}&_sortKeys={sortKeys}")
+  @Headers({
+  "Accept: application/json",
+      "Accept-API-Version: {acceptAPIVersion}",
+
+      "Cookie: {cookie}"
+  })
+  QueryResponse query(@Param("realm") String realm,
+                      @Param("acceptAPIVersion") String acceptAPIVersion,
+                      @Param("cookie") String cookie,
+                      @QueryMap(encoded = true) Map<String, Object> queryParams);
+
+  /**
+   * A convenience class for generating query parameters for the
+   * <code>query</code> method in a fluent style.
+   */
+  public static class QueryQueryParams extends HashMap<String, Object> {
+    public QueryQueryParams fields(final String value) {
+      put("_fields", EncodingUtils.encode(value));
+      return this;
+    }
+    public QueryQueryParams prettyPrint(final Boolean value) {
+      put("_prettyPrint", EncodingUtils.encode(value));
+      return this;
+    }
+    public QueryQueryParams queryFilter(final String value) {
+      put("_queryFilter", EncodingUtils.encode(value));
+      return this;
+    }
+    public QueryQueryParams pageSize(final Integer value) {
+      put("_pageSize", EncodingUtils.encode(value));
+      return this;
+    }
+    public QueryQueryParams totalPagedResultsPolicy(final String value) {
+      put("_totalPagedResultsPolicy", EncodingUtils.encode(value));
+      return this;
+    }
+    public QueryQueryParams sortKeys(final String value) {
+      put("_sortKeys", EncodingUtils.encode(value));
       return this;
     }
   }
