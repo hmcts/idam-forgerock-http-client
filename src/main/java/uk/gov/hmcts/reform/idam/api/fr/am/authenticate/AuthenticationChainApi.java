6de2fad9f108cec1de44772f1ba6a3e6a2525308
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-06-17 14:05:34 +0100

    SIDM-4092 upgrade to java11 bump idam-bom to 2.1.2 bump swagger codegen to 2.4.14 merge two authentication methods

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
@@ -1,53 +1,67 @@
 package uk.gov.hmcts.reform.idam.api.fr.am.authenticate;
 
-import feign.Headers;
-import feign.Param;
-import feign.QueryMap;
-import feign.RequestLine;
-import uk.gov.hmcts.reform.idam.api.fr.am.authenticate.model.AuthenticationChain;
 import uk.gov.hmcts.reform.idam.api.fr.client.invoker.ApiClient;
 import uk.gov.hmcts.reform.idam.api.fr.client.invoker.EncodingUtils;
 
-import java.util.HashMap;
-import java.util.Map;
+import uk.gov.hmcts.reform.idam.api.fr.shared.model.ApiError;
+import uk.gov.hmcts.reform.idam.api.fr.am.authenticate.model.AuthenticationChain;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2019-10-28T12:12:27.878Z")
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import feign.*;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-06-17T13:35:00.855+01:00")
 public interface AuthenticationChainApi extends ApiClient.Api {
 
 
   /**
-   * Submits an authentication chain to AM
-   * POST an authentication chain back to AM
-    * @param realm Parameter specifying the realm. (required)
-    * @param authIndexType Specifies the indexType, such as service (required)
-    * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
-    * @param cookie  (optional)
-    * @param requestPayload  (optional)
+   * Authenticates a user or initiates an authentication flow when MFA is required
+   * POST call to initiate authentication flow
+   * @param myRealm Parameter specifying the realm. (required)
+   * @param authIndexType Specifies the indexType, such as service (required)
+   * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
+   * @param xOpenAMCustUsername Defines the user&#39;s username (required)
+   * @param xOpenAMCustPassword Defines the user&#39;s password (required)
+   * @param xOriginIP Defines the user&#39;s original IP address (required)
+   * @param xRedirectURI Defines the redirectUri for the policies check (required)
+   * @param xAdminUsername Defines the admin username for policies check (required)
+   * @param xAdminPassword Defines the admin password for policies check (required)
    * @return AuthenticationChain
    */
-  @RequestLine("POST /json/realms/{realm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
   @Headers({
-    "Content-Type: application/json",
-    "Accept: application/json",
-    "Cookie: {cookie}"
+          "Accept: application/json",
+          "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+
+          "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+
+          "X-Origin-IP: {xOriginIP}",
+
+          "X-Redirect-URI: {xRedirectURI}",
+
+          "X-Admin-Username: {xAdminUsername}",
+
+          "X-Admin-Password: {xAdminPassword}"
   })
-  AuthenticationChain authenticate(@Param("realm") String realm,
-                                   @Param("authIndexType") String authIndexType,
-                                   @Param("authIndexValue") String authIndexValue,
-                                   @Param("cookie") String cookie,
-                                   AuthenticationChain requestPayload);
+  AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xAdminUsername") String xAdminUsername, @Param("xAdminPassword") String xAdminPassword);
 
   /**
-   * Submits an authentication chain to AM
-   * POST an authentication chain back to AM
+   * Authenticates a user or initiates an authentication flow when MFA is required
+   * POST call to initiate authentication flow
    * Note, this is equivalent to the other <code>authenticate</code> method,
    * but with the query parameters collected into a single Map parameter. This
    * is convenient for services with optional query parameters, especially when
    * used with the {@link AuthenticateQueryParams} class that allows for
    * building up this map in a fluent style.
-   * @param realm Parameter specifying the realm. (required)
-   * @param cookie  (optional)
-   * @param requestPayload  (optional)
+   * @param myRealm Parameter specifying the realm. (required)
+   * @param xOpenAMCustUsername Defines the user&#39;s username (required)
+   * @param xOpenAMCustPassword Defines the user&#39;s password (required)
+   * @param xOriginIP Defines the user&#39;s original IP address (required)
+   * @param xRedirectURI Defines the redirectUri for the policies check (required)
+   * @param xAdminUsername Defines the admin username for policies check (required)
+   * @param xAdminPassword Defines the admin password for policies check (required)
    * @param queryParams Map of query parameters as name-value pairs
    *   <p>The following elements may be specified in the query map:</p>
    *   <ul>
@@ -56,16 +70,22 @@ public interface AuthenticationChainApi extends ApiClient.Api {
    *   </ul>
    * @return AuthenticationChain
    */
-  @RequestLine("POST /json/realms/{realm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
   @Headers({
-  "Content-Type: application/json",
-  "Accept: application/json",
-      "Cookie: {cookie}"
+          "Accept: application/json",
+          "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+
+          "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+
+          "X-Origin-IP: {xOriginIP}",
+
+          "X-Redirect-URI: {xRedirectURI}",
+
+          "X-Admin-Username: {xAdminUsername}",
+
+          "X-Admin-Password: {xAdminPassword}"
   })
-  AuthenticationChain authenticate(@Param("realm") String realm,
-                                   @Param("cookie") String cookie,
-                                   AuthenticationChain requestPayload,
-                                   @QueryMap(encoded = true) Map<String, Object> queryParams);
+  AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xAdminUsername") String xAdminUsername, @Param("xAdminPassword") String xAdminPassword, @QueryMap(encoded=true) Map<String, Object> queryParams);
 
   /**
    * A convenience class for generating query parameters for the
@@ -77,67 +97,6 @@ public interface AuthenticationChainApi extends ApiClient.Api {
       return this;
     }
     public AuthenticateQueryParams authIndexValue(final String value) {
-      put("authIndexValue", EncodingUtils.encode(value));
-      return this;
-    }
-  }
-
-  /**
-   * Initiates an authentication chain with an empty body request
-   * POST call to initiate authentication flow
-    * @param myRealm Parameter specifying the realm. (required)
-    * @param authIndexType Specifies the indexType, such as service (required)
-    * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
-    * @param cookie  (optional)
-   * @return AuthenticationChain
-   */
-  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
-  @Headers({
-    "Accept: application/json",
-    "Cookie: {cookie}"
-  })
-  AuthenticationChain authenticateEmptyBody(@Param("myRealm") String myRealm,
-                                            @Param("authIndexType") String authIndexType,
-                                            @Param("authIndexValue") String authIndexValue,
-                                            @Param("cookie") String cookie);
-
-  /**
-   * Initiates an authentication chain with an empty body request
-   * POST call to initiate authentication flow
-   * Note, this is equivalent to the other <code>authenticateEmptyBody</code> method,
-   * but with the query parameters collected into a single Map parameter. This
-   * is convenient for services with optional query parameters, especially when
-   * used with the {@link AuthenticateEmptyBodyQueryParams} class that allows for
-   * building up this map in a fluent style.
-   * @param myRealm Parameter specifying the realm. (required)
-   * @param cookie  (optional)
-   * @param queryParams Map of query parameters as name-value pairs
-   *   <p>The following elements may be specified in the query map:</p>
-   *   <ul>
-   *   <li>authIndexType - Specifies the indexType, such as service (required)</li>
-   *   <li>authIndexValue - Specifies the identity of the module we are calling, such as hotpChain (required)</li>
-   *   </ul>
-   * @return AuthenticationChain
-   */
-  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
-  @Headers({
-  "Accept: application/json",
-      "Cookie: {cookie}"
-  })
-  AuthenticationChain authenticateEmptyBody(@Param("myRealm") String myRealm,
-                                            @Param("cookie") String cookie,
-                                            @QueryMap(encoded = true) Map<String, Object> queryParams);
-
-  /**
-   * A convenience class for generating query parameters for the
-   * <code>authenticateEmptyBody</code> method in a fluent style.
-   */
-  public static class AuthenticateEmptyBodyQueryParams extends HashMap<String, Object> {
-    public AuthenticateEmptyBodyQueryParams authIndexType(final String value) {
-      put("authIndexType", EncodingUtils.encode(value));
-      return this;
-    }
-    public AuthenticateEmptyBodyQueryParams authIndexValue(final String value) {
       put("authIndexValue", EncodingUtils.encode(value));
       return this;
     }
