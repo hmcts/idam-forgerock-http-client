7ef6a85a8edd9286589b5c2c6c83633f6d364995
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-07-08 11:25:42 +0100

    SIDM-4092 WIP

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
@@ -12,85 +12,107 @@ import java.util.List;
 import java.util.Map;
 import feign.*;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-07-03T12:33:56.808+01:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-07-03T15:16:05.268+01:00")
 public interface AuthenticationChainApi extends ApiClient.Api {
 
 
-  /**
-   * Authenticates a user or initiates an authentication flow when MFA is required
-   * POST call to initiate authentication flow
-   * @param myRealm Parameter specifying the realm. (required)
-   * @param authIndexType Specifies the indexType, such as service (required)
-   * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
-   * @param xOpenAMCustUsername Defines the user&#39;s username (required)
-   * @param xOpenAMCustPassword Defines the user&#39;s password (required)
-   * @param xOriginIP Defines the user&#39;s original IP address (required)
-   * @param xRedirectURI Defines the redirectUri for the policies check (required)
-   * @param requestPayload  (optional)
-   * @return AuthenticationChain
-   */
-  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
-  @Headers({
-          "Content-Type: application/json",
-          "Accept: application/json",
-          "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+    /**
+     * Authenticates a user or initiates an authentication flow if MFA is required
+     * POST call to initiate authentication flow
+     * @param myRealm Parameter specifying the realm. (required)
+     * @param authIndexType Specifies the indexType, such as service (required)
+     * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
+     * @param xOpenAMCustUsername Defines the user&#39;s username (required)
+     * @param xOpenAMCustPassword Defines the user&#39;s password (required)
+     * @param xOriginIP Defines the user&#39;s original IP address (required)
+     * @param xRedirectURI Defines the redirectUri for the policies check (required)
+     * @param xAdminUsername Defines the admin username for policies check (required)
+     * @param xAdminPassword Defines the admin password for policies check (required)
+     * @return AuthenticationChain
+     */
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+    @Headers({
+            "Accept: application/json",
+            "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
 
-          "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+            "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
 
-          "X-Origin-IP: {xOriginIP}",
+            "X-Origin-IP: {xOriginIP}",
 
-          "X-Redirect-URI: {xRedirectURI}"
-  })
-  AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, AuthenticationChain requestPayload);
+            "X-Redirect-URI: {xRedirectURI}",
 
-  /**
-   * Authenticates a user or initiates an authentication flow when MFA is required
-   * POST call to initiate authentication flow
-   * Note, this is equivalent to the other <code>authenticate</code> method,
-   * but with the query parameters collected into a single Map parameter. This
-   * is convenient for services with optional query parameters, especially when
-   * used with the {@link AuthenticateQueryParams} class that allows for
-   * building up this map in a fluent style.
-   * @param myRealm Parameter specifying the realm. (required)
-   * @param xOpenAMCustUsername Defines the user&#39;s username (required)
-   * @param xOpenAMCustPassword Defines the user&#39;s password (required)
-   * @param xOriginIP Defines the user&#39;s original IP address (required)
-   * @param xRedirectURI Defines the redirectUri for the policies check (required)
-   * @param requestPayload  (optional)
-   * @param queryParams Map of query parameters as name-value pairs
-   *   <p>The following elements may be specified in the query map:</p>
-   *   <ul>
-   *   <li>authIndexType - Specifies the indexType, such as service (required)</li>
-   *   <li>authIndexValue - Specifies the identity of the module we are calling, such as hotpChain (required)</li>
-   *   </ul>
-   * @return AuthenticationChain
-   */
-  @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
-  @Headers({
-          "Content-Type: application/json",
-          "Accept: application/json",
-          "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+            "X-Admin-Username: {xAdminUsername}",
 
-          "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+            "X-Admin-Password: {xAdminPassword}"
+    })
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xAdminUsername") String xAdminUsername, @Param("xAdminPassword") String xAdminPassword);
 
-          "X-Origin-IP: {xOriginIP}",
+    /**
+     * Authenticates a user or initiates an authentication flow if MFA is required
+     * POST call to initiate authentication flow
+     * Note, this is equivalent to the other <code>authenticate</code> method,
+     * but with the query parameters collected into a single Map parameter. This
+     * is convenient for services with optional query parameters, especially when
+     * used with the {@link AuthenticateQueryParams} class that allows for
+     * building up this map in a fluent style.
+     * @param myRealm Parameter specifying the realm. (required)
+     * @param xOpenAMCustUsername Defines the user&#39;s username (required)
+     * @param xOpenAMCustPassword Defines the user&#39;s password (required)
+     * @param xOriginIP Defines the user&#39;s original IP address (required)
+     * @param xRedirectURI Defines the redirectUri for the policies check (required)
+     * @param xAdminUsername Defines the admin username for policies check (required)
+     * @param xAdminPassword Defines the admin password for policies check (required)
+     * @param queryParams Map of query parameters as name-value pairs
+     *   <p>The following elements may be specified in the query map:</p>
+     *   <ul>
+     *   <li>authIndexType - Specifies the indexType, such as service (required)</li>
+     *   <li>authIndexValue - Specifies the identity of the module we are calling, such as hotpChain (required)</li>
+     *   </ul>
+     * @return AuthenticationChain
+     */
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
+    @Headers({
+            "Accept: application/json",
+            "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
 
-          "X-Redirect-URI: {xRedirectURI}"
-  })
-  AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, AuthenticationChain requestPayload, @QueryMap(encoded=true) Map<String, Object> queryParams);
+            "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
 
-  /**
-   * A convenience class for generating query parameters for the
-   * <code>authenticate</code> method in a fluent style.
-   */
-  public static class AuthenticateQueryParams extends HashMap<String, Object> {
-    public AuthenticateQueryParams authIndexType(final String value) {
-      put("authIndexType", EncodingUtils.encode(value));
-      return this;
+            "X-Origin-IP: {xOriginIP}",
+
+            "X-Redirect-URI: {xRedirectURI}",
+
+            "X-Admin-Username: {xAdminUsername}",
+
+            "X-Admin-Password: {xAdminPassword}"
+    })
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xAdminUsername") String xAdminUsername, @Param("xAdminPassword") String xAdminPassword, @QueryMap(encoded=true) Map<String, Object> queryParams);
+
+    /**
+     * A convenience class for generating query parameters for the
+     * <code>authenticate</code> method in a fluent style.
+     */
+    public static class AuthenticateQueryParams extends HashMap<String, Object> {
+        public AuthenticateQueryParams authIndexType(final String value) {
+            put("authIndexType", EncodingUtils.encode(value));
+            return this;
+        }
+        public AuthenticateQueryParams authIndexValue(final String value) {
+            put("authIndexValue", EncodingUtils.encode(value));
+            return this;
+        }
     }
-    public AuthenticateQueryParams authIndexValue(final String value) {
-      put("authIndexValue", EncodingUtils.encode(value));
-      return this;
-    }
-  }
+
+    /**
+     * Completes the authentication flow when MFA is required
+     * POST call to initiate authentication flow
+     * @param realm Parameter specifying the realm. (required)
+     * @param requestPayload  (required)
+     * @return AuthenticationChain
+     */
+    @RequestLine("POST /json/realms/{realm}/authenticate")
+    @Headers({
+            "Content-Type: application/json",
+            "Accept: application/json",
+    })
+    AuthenticationChain authenticateSupplyOtp(@Param("realm") String realm, AuthenticationChain requestPayload);
 }
