2b275369dd07be61a01c13c3415daa97d5a64582
Radoslaw Orlowski <radoslaw.orlowski@amido.com>
2020-10-22 11:47:09 +0100

    Revert "SIDM-4824 Remove tokenId param as it will be taken from a cookie."
    This reverts commit 81a2b9b9b1b2b17c0a43f037bf103ad5cf29bed5.

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import feign.*;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-21T11:40:28.748+01:00")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2020-10-16T15:12:54.143+01:00")
 public interface AuthenticationChainApi extends ApiClient.Api {
 
 
@@ -26,6 +26,7 @@ public interface AuthenticationChainApi extends ApiClient.Api {
      * @param xRedirectURI Defines the redirectUri for the policies check (required)
      * @param xOpenAMCustUsername Defines the user&#39;s username (optional)
      * @param xOpenAMCustPassword Defines the user&#39;s password (optional)
+     * @param idamSession Defines the user existing SSO token (optional)
      * @return AuthenticationChain
      */
     @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}")
@@ -35,11 +36,13 @@ public interface AuthenticationChainApi extends ApiClient.Api {
 
             "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
 
+            "Idam.Session: {idamSession}",
+
             "X-Origin-IP: {xOriginIP}",
 
             "X-Redirect-URI: {xRedirectURI}"
     })
-    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword);
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("idamSession") String idamSession);
 
     /**
      * Authenticates a user or initiates an authentication flow if MFA is required
@@ -54,6 +57,7 @@ public interface AuthenticationChainApi extends ApiClient.Api {
      * @param xRedirectURI Defines the redirectUri for the policies check (required)
      * @param xOpenAMCustUsername Defines the user&#39;s username (optional)
      * @param xOpenAMCustPassword Defines the user&#39;s password (optional)
+     * @param idamSession Defines the user existing SSO token (optional)
      * @param queryParams Map of query parameters as name-value pairs
      *   <p>The following elements may be specified in the query map:</p>
      *   <ul>
@@ -69,11 +73,13 @@ public interface AuthenticationChainApi extends ApiClient.Api {
 
             "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
 
+            "Idam.Session: {idamSession}",
+
             "X-Origin-IP: {xOriginIP}",
 
             "X-Redirect-URI: {xRedirectURI}"
     })
-    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @QueryMap(encoded=true) Map<String, Object> queryParams);
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("idamSession") String idamSession, @QueryMap(encoded=true) Map<String, Object> queryParams);
 
     /**
      * A convenience class for generating query parameters for the
