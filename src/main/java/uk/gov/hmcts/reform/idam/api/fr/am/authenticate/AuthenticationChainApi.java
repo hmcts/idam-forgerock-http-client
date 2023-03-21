35fb09a81761226d47eb451e3b0ed5adf0cd12f1
kremi <34029797+kremi@users.noreply.github.com>
2023-03-21 11:22:48 +0000

    ForceAuth parameter
    Default ForceAuth to false (so that we do not create session on every authenticate request) and allow it to be set by the client.

--- src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
+++ src/main/java/uk/gov/hmcts/reform/idam/api/fr/am/authenticate/AuthenticationChainApi.java
@@ -29,7 +29,7 @@ public interface AuthenticationChainApi extends ApiClient.Api {
      * @param idamSession Defines the user existing SSO token (optional)
      * @return AuthenticationChain
      */
-    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth=true")
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth=false")
     @Headers({
             "Accept: application/json",
             "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
@@ -66,7 +66,74 @@ public interface AuthenticationChainApi extends ApiClient.Api {
      *   </ul>
      * @return AuthenticationChain
      */
-    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth=true")
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth=false")
+    @Headers({
+            "Accept: application/json",
+            "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+
+            "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+
+            "Idam.Session: {idamSession}",
+
+            "X-Origin-IP: {xOriginIP}",
+
+            "X-Redirect-URI: {xRedirectURI}"
+    })
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("idamSession") String idamSession, @QueryMap(encoded=true) Map<String, Object> queryParams);
+
+/**
+     * Authenticates a user or initiates an authentication flow if MFA is required
+     * POST call to initiate authentication flow
+     * @param myRealm Parameter specifying the realm. (required)
+     * @param authIndexType Specifies the indexType, such as service (required)
+     * @param authIndexValue Specifies the identity of the module we are calling, such as hotpChain (required)
+     * @param xOriginIP Defines the user&#39;s original IP address (required)
+     * @param xRedirectURI Defines the redirectUri for the policies check (required)
+     * @param forceAuth Forces authentication even if an active session cookie is present. (optional)
+     * @param xOpenAMCustUsername Defines the user&#39;s username (optional)
+     * @param xOpenAMCustPassword Defines the user&#39;s password (optional)
+     * @param idamSession Defines the user existing SSO token (optional)
+     * @return AuthenticationChain
+     */
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth={forceAuth}")
+    @Headers({
+            "Accept: application/json",
+            "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
+
+            "X-OpenAM-Cust-Password: {xOpenAMCustPassword}",
+
+            "Idam.Session: {idamSession}",
+
+            "X-Origin-IP: {xOriginIP}",
+
+            "X-Redirect-URI: {xRedirectURI}"
+    })
+    AuthenticationChain authenticate(@Param("myRealm") String myRealm, @Param("authIndexType") String authIndexType, @Param("authIndexValue") String authIndexValue, @Param("xOriginIP") String xOriginIP, @Param("xRedirectURI") String xRedirectURI, @Param("forceAuth") String forceAuth, @Param("xOpenAMCustUsername") String xOpenAMCustUsername, @Param("xOpenAMCustPassword") String xOpenAMCustPassword, @Param("idamSession") String idamSession);
+
+    /**
+     * Authenticates a user or initiates an authentication flow if MFA is required
+     * POST call to initiate authentication flow
+     * Note, this is equivalent to the other <code>authenticate</code> method,
+     * but with the query parameters collected into a single Map parameter. This
+     * is convenient for services with optional query parameters, especially when
+     * used with the {@link AuthenticateQueryParams} class that allows for
+     * building up this map in a fluent style.
+     * @param myRealm Parameter specifying the realm. (required)
+     * @param xOriginIP Defines the user&#39;s original IP address (required)
+     * @param xRedirectURI Defines the redirectUri for the policies check (required)
+     * @param xOpenAMCustUsername Defines the user&#39;s username (optional)
+     * @param xOpenAMCustPassword Defines the user&#39;s password (optional)
+     * @param idamSession Defines the user existing SSO token (optional)
+     * @param queryParams Map of query parameters as name-value pairs
+     *   <p>The following elements may be specified in the query map:</p>
+     *   <ul>
+     *   <li>authIndexType - Specifies the indexType, such as service (required)</li>
+     *   <li>authIndexValue - Specifies the identity of the module we are calling, such as hotpChain (required)</li>
+     *   <li>forceAuth - Forces authentication even if an active session cookie is present. (optional)</li>
+     *   </ul>
+     * @return AuthenticationChain
+     */
+    @RequestLine("POST /json/realms/{myRealm}/authenticate?authIndexType={authIndexType}&authIndexValue={authIndexValue}&ForceAuth={forceAuth}")
     @Headers({
             "Accept: application/json",
             "X-OpenAM-Cust-Username: {xOpenAMCustUsername}",
@@ -92,6 +159,10 @@ public interface AuthenticationChainApi extends ApiClient.Api {
         }
         public AuthenticateQueryParams authIndexValue(final String value) {
             put("authIndexValue", EncodingUtils.encode(value));
+            return this;
+        }
+        public AuthenticateQueryParams forceAuth(final String value) {
+            put("ForceAuth", EncodingUtils.encode(value));
             return this;
         }
     }
